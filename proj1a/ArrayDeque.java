d7d8b5ab871a57832cda88314cb609ebacef2abe
nicolaswangcrypto <rush.rondo@gmail.com>
2023-02-07 14:14:57 +0800

    Finish: ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,56 +1,122 @@
 public class ArrayDeque<T> {
     private T[] items;
-    private int left;
-    private int right;
-    private int capacity = 8;
+    private int nextFirst;
+    private int nextLast;
+    private int size;
+    private final int INITIAL_CAPACITY = 8;
 
     public ArrayDeque() {
-        items = (T[]) new Object[capacity];
-        left = right = 0;
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        nextFirst = 0;
+        nextLast = 1;
+        size = 0;
     }
 
     public int size() {
-        return (right - left + capacity) % capacity;
-    }
-
-    public boolean isFull() {
-        return size() == capacity - 1;
+        return size;
     }
 
     public boolean isEmpty() {
-        return left == right;
+        return (size == 0 ? true : false);
     }
 
-    private void resize(int newSize) {
-        T[] newArray = (T[]) new Object[newSize];
-        int size = size();
-        int j = 0;
+    public int minusOne(int index) {
+        return Math.floorMod(index - 1, items.length);
+    }
 
-        for (int i = 0; i < size; i++) {
-            newArray[i] = items[j];
-            j++;
+    public int plusOne(int index) {
+        return Math.floorMod(index + 1, items.length);
+    }
+
+    public int plusOne(int index, int length) {
+        return Math.floorMod(index + 1, length);
+    }
+
+    private void resizeHelper(int capacity) {
+        T[] temp = items;
+        int begin = plusOne(nextFirst); // nextFirst 的前一個位置
+        int end = minusOne(nextLast); // nextLast 的前一個位置
+        items = (T[]) new Object[capacity];
+        nextFirst = 0;
+        nextLast = 1;
+        for (int i = begin; i != end; i = plusOne(i, temp.length)) {
+            items[nextLast] = temp[i];
+            nextLast = plusOne(nextLast);
         }
-        left = 0;
-        right = size;
-        items = newArray;
-        capacity = newSize;
+        items[nextLast] = temp[end];
+        nextLast = plusOne(nextLast);
+    }
+
+    private void expand() {
+        resizeHelper(items.length * 2);
+    }
+
+    private void reduce() {
+        resizeHelper(items.length / 2);
+    }
+
+    private void resize() {
+        if (size == items.length) {
+            expand();
+        }
+        if (size < items.length / 4 && items.length >= 16) {
+            reduce();
+        }
     }
 
     public void addFirst(T item) {
-        if (isFull()) {
-            resize((int) (capacity * 1.5));
-        }
+        resize();
+        items[nextFirst] = item;
+        nextFirst = minusOne(nextFirst);
+        size++;
+    }
 
-        left = (left - 1 + capacity) % capacity;
-        items[left] = item;
+    public T getFirst() {
+        return items[plusOne(nextFirst)];
+    }
+
+    public T removeFirst() {
+        resize();
+        T res = getFirst();
+        nextFirst = plusOne(nextFirst);
+        items[nextFirst] = null;
+        size--;
+        return res;
     }
 
     public void addLast(T item) {
-        if (isFull()) {
-            resize((int) (capacity * 1.5));
-        }
+        resize();
+        items[nextLast] = item;
+        nextLast = plusOne(nextLast);
+        size++;
+    }
 
-        items[right] = item;
-        right = (right + 1 + capacity) % capacity;
+    public T getLast() {
+        return items[minusOne(nextLast)];
+    }
+
+    public T removeLast() {
+        resize();
+        T res = getLast();
+        nextLast = minusOne(nextLast);
+        items[nextLast] = null;
+        size--;
+        return res;
+    }
+
+    public void printDeque() {
+        for (int i = plusOne(nextFirst); i != nextLast; i = plusOne(i)) {
+            System.out.print(items[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        index = Math.floorMod(plusOne(nextFirst) + index, items.length);
+        return items[index];
     }
 }
